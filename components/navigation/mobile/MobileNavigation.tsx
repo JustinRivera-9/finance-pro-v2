4b9018db928e5ffb74c9e347a7c608255e0c8a91
Justin Rivera <justin_rivera9@outlook.com>
2024-09-15 20:30:51 -0600

    Fixed issue with active link

--- components/navigation/mobile/MobileNavigation.tsx
+++ components/navigation/mobile/MobileNavigation.tsx
@@ -1,24 +1,42 @@
+"use client";
 import Link from "next/link";
 import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
 import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 import PaidIcon from "@mui/icons-material/Paid";
+import { usePathname } from "next/navigation";
 
 const MobileNavigation = () => {
+  const path = usePathname();
+
   return (
-    <ul className="fixed bottom-0 left-0 right-0 flex justify-between mt-6 px-8 py-4 bg-[#313233] border-t-2 border-slate-500">
-      <Link
-        href="/dashboard"
-        className="hover:text-secondary transition-colors"
-      >
-        <SpaceDashboardIcon fontSize="large" />
-      </Link>
-      <Link href="/planned" className="hover:text-secondary transition-colors">
-        <AppRegistrationIcon fontSize="large" />
-      </Link>
-      <Link href="/spent" className="hover:text-secondary transition-colors">
-        <PaidIcon fontSize="large" />
-      </Link>
-    </ul>
+    <nav className="fixed bottom-0 left-0 right-0 mt-6 py-4 bg-[#313233] border-t-2 border-slate-500">
+      <ul className="flex justify-around">
+        <li>
+          <Link
+            href="/dashboard"
+            className={path.startsWith("/dashboard") ? "text-accent" : ""}
+          >
+            <SpaceDashboardIcon fontSize="large" />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/planned"
+            className={path.startsWith("/planned") ? "text-accent" : ""}
+          >
+            <AppRegistrationIcon fontSize="large" />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/spent"
+            className={path.startsWith("/spent") ? "text-accent" : ""}
+          >
+            <PaidIcon fontSize="large" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 export default MobileNavigation;
