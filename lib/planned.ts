0a1e483cc7333832f674bf8d9f19fdf9c494fbc8
Justin Rivera <justin_rivera9@outlook.com>
2024-09-15 19:50:09 -0600

    Added shadcn ui component

--- lib/planned.ts
+++
@@ -1,8 +0,0 @@
-import sql from "better-sqlite3";
-
-const db = sql("planned");
-
-export const getPlannedCategories = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  return db.prepare("SELECT * FROM categories").all();
-};
