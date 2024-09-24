fc811f4d4fe15b3e210c69f5424f2ffdddfd613f
Justin Rivera <justin_rivera9@outlook.com>
2024-09-24 00:17:32 -0600

    Added fetching categories from DB

--- app/app/planned/page.tsx
+++ app/app/planned/page.tsx
@@ -1,116 +1,12 @@
 import Summary from "../../../components/pages/plannedPage/Summary";
 import CategorySection from "../../../components/pages/plannedPage/CategorySection";
 import AddCategoryForm from "../../../components/pages/plannedPage/addCategory/AddCategoryForm";
-
-///// TEMP DATA
-const planned = {
-  isSetUp: true,
-  categories: [
-    {
-      id: 1,
-      category: "Mortgage",
-      amount: 1500,
-      type: "expense",
-      isFixed: true,
-      date: "4",
-    },
-    {
-      id: 2,
-      category: "Going out",
-      amount: 250,
-      type: "expense",
-      isFixed: false,
-    },
-    // {
-    //   id: 3,
-    //   category: "Income",
-    //   amount: 1000,
-    //   type: "income",
-    // },
-    {
-      id: 4,
-      category: "Going out",
-      amount: 250,
-      type: "expense",
-      isFixed: false,
-    },
-    // {
-    //   id: 5,
-    //   category: "Income",
-    //   amount: 1000,
-    //   type: "income",
-    // },
-    {
-      id: 6,
-      category: "Going out",
-      amount: 250,
-      type: "expense",
-      isFixed: false,
-    },
-    {
-      id: 7,
-      category: "Income",
-      amount: 10000,
-      type: "income",
-    },
-    {
-      id: 8,
-      category: "Going out",
-      amount: 250,
-      type: "expense",
-      isFixed: false,
-    },
-    {
-      id: 9,
-      category: "Income",
-      amount: 1000,
-      type: "income",
-    },
-    {
-      id: 10,
-      category: "Car Payment",
-      amount: 1500,
-      type: "expense",
-      isFixed: true,
-      date: "17",
-    },
-    {
-      id: 11,
-      category: "Mortgage",
-      amount: 1500,
-      type: "expense",
-      isFixed: true,
-      date: "22",
-    },
-    {
-      id: 14,
-      category: "Mortgage",
-      amount: 1500,
-      type: "expense",
-      isFixed: true,
-      date: "22",
-    },
-    {
-      id: 12,
-      category: "Student Loans",
-      amount: 1500,
-      type: "expense",
-      isFixed: true,
-      date: "8",
-    },
-    {
-      id: 13,
-      category: "Youtube Music",
-      amount: 1500,
-      type: "expense",
-      isFixed: true,
-      date: "30",
-    },
-  ],
-};
+import { getCategories } from "./actions";
+import type { PlannedCategories } from "@/lib/types";
 
 const PlannedPage = async () => {
-  const { categories } = planned;
+  // const { categories } = planned;
+  const categories: PlannedCategories[] = await getCategories();
 
   return (
     <div className="flex flex-col gap-4">
