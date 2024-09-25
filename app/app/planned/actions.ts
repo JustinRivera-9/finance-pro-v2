b7523cd997017c8ae0dcd224ce1ba9d6714e40fc
Justin Rivera <justin_rivera9@outlook.com>
2024-09-25 00:20:40 -0600

    Added action for adding category

--- app/app/planned/actions.ts
+++ app/app/planned/actions.ts
@@ -1,10 +1,13 @@
 "use server";
+import { v4 as uuid } from "uuid";
 
 import { getUser } from "@/lib/supabase/actions";
 import { createClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+import { CategoryFormData } from "@/lib/types";
+const supabase = createClient();
 
 export const getCategories = async () => {
-  const supabase = createClient();
   const user_id = await getUser();
 
   let { data: categories, error } = await supabase
@@ -15,3 +18,50 @@ export const getCategories = async () => {
   if (error) return error;
   return categories;
 };
+
+export const addCategoryAction = async (formData: FormData) => {
+  const user_id = await getUser();
+  const formFields = Object.fromEntries(formData);
+  // console.log(formFields);
+
+  const { isFixed, type } = formFields;
+  let cleanData: CategoryFormData;
+
+  if (type === "income" || !isFixed) {
+    cleanData = {
+      ...formFields,
+      date: "",
+      isFixed: false,
+    };
+  } else {
+    cleanData = {
+      ...formFields,
+      isFixed: true,
+    };
+  }
+
+  const { error } = await supabase
+    .from("categories")
+    .insert([
+      {
+        type: cleanData.type,
+        category: cleanData.category,
+        amount: cleanData.amount,
+        date: cleanData.date,
+        isFixed: cleanData.isFixed,
+        id: uuid(),
+        user_id,
+      },
+    ])
+    .eq("user_id", user_id);
+
+  if (error) {
+    console.log(error);
+  }
+
+  revalidatePath("app/planned");
+};
+
+export const updateCategory = async () => {};
+
+export const deleteCategory = async () => {};
