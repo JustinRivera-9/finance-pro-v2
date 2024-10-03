c8e5a9da360b3b6c3c89e246e7ce1a274ca82fdf
Justin Rivera <justin_rivera9@outlook.com>
2024-10-02 23:38:39 -0600

    Moved file location

---
+++ types/types.ts
@@ -0,0 +1,32 @@
+import { UUID } from "crypto";
+
+export type PlannedCategories = {
+  id: number;
+  category: string;
+  amount: number;
+  type: string;
+  isFixed: boolean;
+  date: string;
+  user_id: string;
+};
+
+export type CategoryFormData = {
+  amount: string;
+  category: string;
+  isFixed: boolean;
+  type: string;
+  date: string;
+  id: string;
+  user_id: string;
+};
+
+export type ToastActionResults = {
+  success: boolean;
+  message: string;
+};
+
+// PLAID TYPES
+export type ExchangePublicTokenProps = {
+  publicToken: string;
+  user: any;
+};
