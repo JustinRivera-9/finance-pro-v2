d2981f6d00054402c86dce20c55a8f9731ace17e
Justin Rivera <justin_rivera9@outlook.com>
2024-10-02 23:16:06 -0600

    added check for categories not set up

--- app/app/planned/page.tsx
+++ app/app/planned/page.tsx
@@ -3,9 +3,22 @@ import CategorySection from "../../../components/pages/plannedPage/CategorySecti
 import AddCategoryForm from "../../../components/pages/plannedPage/addCategory/AddCategoryForm";
 import { getCategories } from "./actions";
 import type { PlannedCategories } from "@/lib/types";
+import EmptyMessage from "@/components/ui/EmptyMessage";
 
 const PlannedPage = async () => {
   const categories: any = await getCategories();
+
+  if (!categories.length) {
+    return (
+      <section className="flex flex-col items-center text-center gap-4">
+        <EmptyMessage
+          title="Set Up Your Categories"
+          description="Here is where you can easily manage the categories you want to track."
+        />
+        <AddCategoryForm />
+      </section>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-4">
