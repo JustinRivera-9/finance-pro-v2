10d0dd2cff5683beae1209db334fa6b3cb975c7c
Justin Rivera <justin_rivera9@outlook.com>
2024-10-02 23:17:22 -0600

    Added check for empty categories

--- components/pages/plannedPage/CategoryColumn.tsx
+++ components/pages/plannedPage/CategoryColumn.tsx
@@ -1,5 +1,6 @@
 import { type PlannedCategories } from "@/lib/types";
 import CategoryCard from "./CategoryCard";
+import EmptyMessage from "@/components/ui/EmptyMessage";
 
 type CategoryColumnProps = {
   category: PlannedCategories[];
@@ -8,16 +9,21 @@ type CategoryColumnProps = {
 
 const CategoryColumn: React.FC<CategoryColumnProps> = ({ category, title }) => {
   const isFixed = title === "FIXED EXPENSES";
+  const isEmpty = !category.length;
 
   return (
     <div className="flex flex-col gap-2 w-full">
       <p className="text-accent text-2xl font-semibold">{title}</p>
       <ul className="flex flex-col flex-wrap gap-4 justify-start mx-auto w-full px-8">
-        {category.map((category) => (
-          <li key={category.id}>
-            <CategoryCard categoryData={category} />
-          </li>
-        ))}
+        {isEmpty ? (
+          <EmptyMessage description={`Add your first ${title} catgory`} />
+        ) : (
+          category.map((category) => (
+            <li key={category.id}>
+              <CategoryCard categoryData={category} />
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
