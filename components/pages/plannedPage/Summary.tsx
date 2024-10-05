d963bb620b4073eb8fd2534a8c7937f8aa968ef4
Justin Rivera <justin_rivera9@outlook.com>
2024-10-05 14:18:07 -0600

    Added currency format functions

--- components/pages/plannedPage/Summary.tsx
+++ components/pages/plannedPage/Summary.tsx
@@ -1,30 +1,49 @@
 import { calcTotalPlanned } from "@/lib/calculations";
-import { PlannedCategories } from "@/types/types";
+import { formatCurrency } from "@/lib/utils";
+import type { CategoryData } from "@/types/types";
 
-type SummaryProps = { categories: PlannedCategories[] };
+type SummaryProps = { categories: CategoryData[] };
 
 const Summary: React.FC<SummaryProps> = ({ categories }) => {
   const totalExpenses = calcTotalPlanned(categories, "expense");
   const totalIncome = calcTotalPlanned(categories, "income");
   const underBudget = totalIncome > totalExpenses;
-  const budgetDelta = totalIncome - totalExpenses;
+  const budgetDelta = Math.abs(totalIncome - totalExpenses);
 
   return (
     <div className="flex flex-col gap-4">
       <div className="flex font-semibold gap-6 text-accent justify-center">
         <div className="bg-card px-2 py-2 rounded-lg text-left w-2/5">
           <p className="text-lg text-light">Income</p>
-          <p className="text-3xl">${totalIncome ? totalIncome : 0}</p>
+          <p className="text-3xl">
+            {totalIncome
+              ? formatCurrency(totalIncome, true)
+              : formatCurrency(0, true)}
+          </p>
         </div>
         <div className="bg-card px-4 py-2 rounded-lg text-left w-2/5">
           <p className="text-lg text-light">Expenses</p>
-          <p className="text-3xl">${totalExpenses ? totalExpenses : 0}</p>
+          <p className="text-3xl">
+            {totalExpenses
+              ? formatCurrency(totalExpenses, true)
+              : formatCurrency(0, true)}
+          </p>
         </div>
       </div>
-      <p className="text-md w-4/5 text-center border-card border-2 mx-auto px-2 py-2 rounded-md">
+      <p
+        className={`${
+          !underBudget && "text-error"
+        } text-md w-4/5 text-center border-card border-2 mx-auto px-2 py-2 rounded-md`}
+      >
         {underBudget
-          ? `Doing great, you're on track to save $${budgetDelta} every month!`
-          : `Looks like you're planning to spend $${budgetDelta} more than you're income.`}
+          ? `Doing great, you're on track to save ${formatCurrency(
+              budgetDelta,
+              true
+            )} every month!`
+          : `Looks like you're planning to spend ${formatCurrency(
+              budgetDelta,
+              true
+            )} more than you're income.`}
       </p>
     </div>
   );
