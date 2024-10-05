9d5218d621acfa95b2cb977bba622aef97e078ae
Justin Rivera <justin_rivera9@outlook.com>
2024-10-05 13:17:13 -0600

    init

---
+++ components/pages/plannedPage/EditButton.tsx
@@ -0,0 +1,24 @@
+import EditIcon from "@mui/icons-material/Edit";
+import CategoryForm from "./addCategory/CategoryForm";
+import FormDrawer from "@/components/ui/DrawerCard";
+import type { CategoryData } from "@/types/types";
+
+type EditButtonProps = {
+  categoryData: CategoryData;
+};
+
+const EditButton = ({ categoryData }: EditButtonProps) => {
+  return (
+    <div>
+      <FormDrawer
+        title={`Edit ${categoryData.category} Category`}
+        triggerLabel={<EditIcon />}
+        color="light"
+      >
+        <CategoryForm categoryData={categoryData} />
+      </FormDrawer>
+    </div>
+  );
+};
+
+export default EditButton;
