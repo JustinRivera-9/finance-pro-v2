7224edb2a2f1e7e50fbea73b9d46165624487151
Justin Rivera <justin_rivera9@outlook.com>
2024-10-05 13:02:31 -0600

    Added color prop

--- components/pages/plannedPage/addCategory/AddCategoryForm.tsx
+++ components/pages/plannedPage/addCategory/AddCategoryForm.tsx
@@ -1,16 +1,13 @@
 import FormDrawer from "../../../ui/DrawerCard";
 import CategoryForm from "./CategoryForm";
 
-type AddCategoryFormProps = {
-  open?: boolean;
-};
-
-const AddCategoryForm = ({ open = false }: AddCategoryFormProps) => {
+const AddCategoryForm = () => {
   return (
     <FormDrawer
       description="Create a new budget category to track expenses."
       title="Add Category"
       triggerLabel={<p className="text-xl font-semibold py-2">Add Category</p>}
+      color="accent"
     >
       <CategoryForm />
     </FormDrawer>
