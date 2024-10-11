b72eac634981e060bc10f7e3062bd7e333d601a1
Justin Rivera <justin_rivera9@outlook.com>
2024-10-10 19:24:36 -0600

    Added try catch block to getExpenses

--- app/app/spent/actions.ts
+++ app/app/spent/actions.ts
@@ -1,10 +1,20 @@
 "use server";
 import { createClient } from "@/lib/supabase/server";
+import type { GetExpensesResponse } from "@/types/types";
 
 const supabase = createClient();
 
-export const getExpenses = async () => {
-  let { data: expenses, error } = await supabase.from("expenses").select("*");
+export const getExpenses = async (): Promise<GetExpensesResponse> => {
+  try {
+    let { data: expenses, error } = await supabase.from("expenses").select("*");
 
-  return { expenses, error };
+    if (error) throw Error;
+
+    return { expenses, error: null };
+  } catch (err) {
+    return {
+      expenses: null,
+      error: "There was an error getting expenses. Please try again",
+    };
+  }
 };
