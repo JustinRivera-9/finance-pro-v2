457c9354efca6b779205ec33df4c41407ec8fbea
Justin Rivera <justin_rivera9@outlook.com>
2024-10-10 19:25:11 -0600

    Conditionally shows ExpenseSection

--- app/app/spent/page.tsx
+++ app/app/spent/page.tsx
@@ -1,9 +1,18 @@
-const SpentPage = () => {
+import Summary from "@/components/pages/spent/Summary";
+import { getExpenses } from "./actions";
+import ExpenseSection from "@/components/pages/spent/ExpenseSection";
+
+const SpentPage = async () => {
+  const { expenses, error } = await getExpenses();
+
+  if (error) {
+    return <div>Error: {error.toString()}</div>;
+  }
+
   return (
     <section>
-      <div>Header Section</div>
-      <div>Chart Section</div>
-      <div>Expenses Section</div>
+      <Summary />
+      {expenses && <ExpenseSection expenses={expenses} />}
     </section>
   );
 };
