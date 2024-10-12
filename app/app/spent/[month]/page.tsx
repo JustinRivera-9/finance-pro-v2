c16d26a26244d0ed009999f9d80aaeef37a0e341
Justin Rivera <justin_rivera9@outlook.com>
2024-10-11 23:53:49 -0600

    Added dynamic route segment to filter by month

---
+++ app/app/spent/[month]/page.tsx
@@ -0,0 +1,41 @@
+import ExpenseSection from "@/components/pages/spent/ExpenseSection";
+import Summary from "@/components/pages/spent/Summary";
+import { createClient } from "@/lib/supabase/server";
+import { GetExpensesResponse } from "@/types/types";
+
+const ExpensesPage = async ({ params }: { params: { month: string } }) => {
+  const getExpenses = async (): Promise<GetExpensesResponse> => {
+    const supabase = createClient();
+    try {
+      let { data: expenses, error } = await supabase
+        .from("expenses")
+        .select("*");
+
+      if (error) throw Error;
+
+      console.log(expenses);
+      return { expenses, error: null };
+    } catch (err) {
+      return {
+        expenses: null,
+        error: "There was an error getting expenses. Please try again",
+      };
+    }
+  };
+
+  const { expenses, error } = await getExpenses();
+
+  if (error)
+    return <div>There was an error getting expenses. Please try again</div>;
+
+  return (
+    expenses && (
+      <>
+        <Summary />
+        <ExpenseSection expenses={expenses} month={params.month} />
+      </>
+    )
+  );
+};
+
+export default ExpensesPage;
