7fefe3d54b79e0ce5fdc1e564c043aef5ebe251a
Justin Rivera <justin_rivera9@outlook.com>
2024-10-12 00:11:49 -0600

    init

---
+++ components/pages/spent/NoExpenseMessage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoExpenseMessage = () => {
+  return (
+    <section>
+      <h1>No expenses this month!</h1>
+      <button>Add Expense</button>
+    </section>
+  );
+};
+
+export default NoExpenseMessage;
