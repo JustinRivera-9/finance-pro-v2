85be740e68f96ab32ed3ea50336c49d79c9879f7
Justin Rivera <justin_rivera9@outlook.com>
2024-10-11 23:58:11 -0600

    removed getExpenses

--- app/app/spent/actions.ts
+++ app/app/spent/actions.ts
@@ -1,20 +1,4 @@
 "use server";
 import { createClient } from "@/lib/supabase/server";
-import type { GetExpensesResponse } from "@/types/types";
 
 const supabase = createClient();
-
-export const getExpenses = async (): Promise<GetExpensesResponse> => {
-  try {
-    let { data: expenses, error } = await supabase.from("expenses").select("*");
-
-    if (error) throw Error;
-
-    return { expenses, error: null };
-  } catch (err) {
-    return {
-      expenses: null,
-      error: "There was an error getting expenses. Please try again",
-    };
-  }
-};
