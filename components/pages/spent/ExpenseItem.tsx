4c118d43d09eb677a08858f18916df13bb7767f1
Justin Rivera <justin_rivera9@outlook.com>
2024-10-13 23:51:12 -0600

    Added date format function

--- components/pages/spent/ExpenseItem.tsx
+++ components/pages/spent/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import { formatCurrency, capitalize } from "@/lib/utils";
+import { formatCurrency, formatExpenseDate } from "@/lib/utils";
 import { Expense } from "@/types/types";
 import React from "react";
 
@@ -10,7 +10,7 @@ const ExpenseItem = ({ expense }: ExpenseItemProps) => {
   const { date, amount, description } = expense;
   return (
     <section className="flex gap-4 py-2 justify-start px-4 border-t border-light/30">
-      <p>{capitalize(date)}</p>
+      <p>{formatExpenseDate(date)}</p>
       <p>{formatCurrency(amount, true)}</p>
       <p>{description}</p>
     </section>
