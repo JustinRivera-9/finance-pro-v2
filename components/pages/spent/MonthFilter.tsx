8d0b261915aefed5f5ca9c048fb354f0eb75b5fa
Justin Rivera <justin_rivera9@outlook.com>
2024-10-14 21:09:58 -0600

    Added routing on monthFilter change

--- components/pages/spent/MonthFilter.tsx
+++ components/pages/spent/MonthFilter.tsx
@@ -1,7 +1,61 @@
-const MonthFilter = () => {
-  // When user sleects a new month it should redirect to /app/spent/{month}
+"use client";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+} from "@/components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-  return <div>MonthFilter</div>;
+const MonthFilter = ({ month }: { month: string }) => {
+  const [monthFilter, setMonthFilter] = useState<string>(month);
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const url = pathname.split("/");
+    url.pop();
+    url.push(monthFilter);
+    const updatedPath = url.join("/");
+    router.push(updatedPath);
+  }, [monthFilter]);
+
+  const handleMonthChange = (month: string) => {
+    setMonthFilter(month);
+  };
+
+  return (
+    <Select
+      onValueChange={(month) => handleMonthChange(month)}
+      defaultValue={monthFilter}
+    >
+      <SelectTrigger className="w-fit">
+        <SelectValue className="px-4" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Filter by Month</SelectLabel>
+          <SelectItem value="january">January</SelectItem>
+          <SelectItem value="february">February</SelectItem>
+          <SelectItem value="march">March</SelectItem>
+          <SelectItem value="april">April</SelectItem>
+          <SelectItem value="may">May</SelectItem>
+          <SelectItem value="june">June</SelectItem>
+          <SelectItem value="july">July</SelectItem>
+          <SelectItem value="august">August</SelectItem>
+          <SelectItem value="september">September</SelectItem>
+          <SelectItem value="october">October</SelectItem>
+          <SelectItem value="november">November</SelectItem>
+          <SelectItem value="december">December</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default MonthFilter;
