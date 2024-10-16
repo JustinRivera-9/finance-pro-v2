4d8284f070971f96487b30f3156630abd7b9d252
Justin Rivera <justin_rivera9@outlook.com>
2024-10-15 21:11:34 -0600

    Updated style

--- components/pages/spent/CategoryHeader.tsx
+++ components/pages/spent/CategoryHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CategoryHeader = ({ category }: { category: string }) => {
-  return <h1>{category}</h1>;
+  return <h1 className="text-xl">{category}</h1>;
 };
 
 export default CategoryHeader;
