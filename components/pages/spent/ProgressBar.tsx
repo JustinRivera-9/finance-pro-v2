eb9f1f255074555dacf941f83231395ae788754b
Justin Rivera <justin_rivera9@outlook.com>
2024-10-15 21:10:46 -0600

    Updated styles

--- components/pages/spent/ProgressBar.tsx
+++ components/pages/spent/ProgressBar.tsx
@@ -7,8 +7,8 @@ type ProgressBarProps = {
 
 const ProgressBar = ({ expenses }: ProgressBarProps) => {
   return (
-    <div className="w-full border border-card rounded-full">
-      <div className="h-8 rounded-2xl bg-accent w-[50%] text-dark font-bold text-right pr-6">
+    <div className="w-full border border-card rounded-full bg-light/80">
+      <div className="flex items-center justify-end h-6 rounded-2xl bg-accent w-[50%] text-dark font-bold text-right pr-6">
         {formatCurrency(52.45, true)}
       </div>
     </div>
