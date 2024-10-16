52ba15712abd4bb9c49e305601594e4acba12111
Justin Rivera <justin_rivera9@outlook.com>
2024-10-15 21:32:04 -0600

    Updated styles

--- components/pages/spent/ExpenseTableHeader.tsx
+++ components/pages/spent/ExpenseTableHeader.tsx
@@ -1,5 +1,11 @@
 const ExpenseTableHeader = () => {
-  return <div>ExpenseTableHeader</div>;
+  return (
+    <div className="grid grid-cols-[1fr_1fr_2fr] gap-2 justify-between px-2 text-secondary">
+      <p>Date</p>
+      <p>Amount</p>
+      <p>Description</p>
+    </div>
+  );
 };
 
 export default ExpenseTableHeader;
