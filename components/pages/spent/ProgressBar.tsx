61b1914b384245bc8f70806139ae204b00f2ccd6
Justin Rivera <justin_rivera9@outlook.com>
2024-10-27 16:27:04 -0600

    Updated styling to indicate over/under budget

--- components/pages/spent/ProgressBar.tsx
+++ components/pages/spent/ProgressBar.tsx
@@ -1,15 +1,31 @@
-import { formatCurrency } from "@/lib/utils";
+import { formatCurrency, reduceArr } from "@/lib/utils";
 import { Expense } from "@/types/types";
 
 type ProgressBarProps = {
   expenses: Expense[];
+  budget: number;
 };
 
-const ProgressBar = ({ expenses }: ProgressBarProps) => {
+const ProgressBar = ({ expenses, budget }: ProgressBarProps) => {
+  const totalExpenses = reduceArr(expenses);
+  const percentToBudget = (totalExpenses / budget) * 100;
+  const width = percentToBudget > 100 ? 100 : (totalExpenses / budget) * 100;
+
+  const underBudget = percentToBudget > 100 ? false : true;
+
+  console.log(underBudget);
+
   return (
     <div className="w-full border border-card rounded-full bg-light/80">
-      <div className="flex items-center justify-end h-6 rounded-2xl bg-accent w-[50%] text-dark font-bold text-right pr-6">
-        {formatCurrency(52.45, true)}
+      <div
+        className={`flex items-center justify-end h-6 rounded-2xl bg-accent text-dark font-bold text-right pr-6`}
+        style={{
+          width: `${width}%`,
+          backgroundColor: `${underBudget ? "#84cc16" : "#f87272"}`,
+          color: `${percentToBudget > 20 ? "#262525" : "transparent"}`,
+        }}
+      >
+        {formatCurrency(totalExpenses, true)}
       </div>
     </div>
   );
