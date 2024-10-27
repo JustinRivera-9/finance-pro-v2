778e592ce939f97d0948111eefeea722114a705f
Justin Rivera <justin_rivera9@outlook.com>
2024-10-27 16:10:49 -0600

    Added planned budget total

--- components/pages/spent/CategoryHeader.tsx
+++ components/pages/spent/CategoryHeader.tsx
@@ -1,7 +1,19 @@
+import { formatCurrency } from "@/lib/utils";
 import React from "react";
 
-const CategoryHeader = ({ category }: { category: string }) => {
-  return <h1 className="text-xl">{category}</h1>;
+const CategoryHeader = ({
+  category,
+  budget,
+}: {
+  category: string;
+  budget: number;
+}) => {
+  return (
+    <div className="flex justify-between w-full items-center">
+      <h1 className="text-xl">{category}</h1>
+      <p className="text-md text-secondary">{formatCurrency(budget, true)}</p>
+    </div>
+  );
 };
 
 export default CategoryHeader;
