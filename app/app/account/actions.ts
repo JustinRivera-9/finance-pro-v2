0e7a9bbd127a8009f142a295913acb5f8194d0af
Justin Rivera <justin_rivera9@outlook.com>
2024-10-30 22:00:42 -0600

    Created personal information action

--- app/app/account/actions.ts
+++ app/app/account/actions.ts
@@ -2,38 +2,53 @@
 
 import { getUser } from "@/lib/supabase/actions";
 import { createClient } from "@/lib/supabase/server";
-import type {
-  ContactForm,
-  PasswordForm,
-  PreferencesForm,
-  PersonalInfoForm,
-} from "@/types/accountFormActions";
-const supabase = createClient();
+import { revalidatePath } from "next/cache";
 
-export const getAccountSettings = async () => {
-  // Use promise method to do multiple fetches and wait until they all resolve
-};
-
-export const updatePersonalInfoAction = async ({ email }: PersonalInfoForm) => {
+export const updatePersonalInfoAction = async (formData: FormData) => {
+  const supabase = createClient();
   const user_id = await getUser();
 
-  const { data, error } = await supabase.auth.updateUser({
-    email: email,
-    data: { hello: "world" },
-  });
+  const formFields = Object.fromEntries(formData);
+  const { fullName, email } = formFields;
+
+  const updateEmail = async () => {
+    const { error: emailError } = await supabase.auth.updateUser({
+      email: email as string,
+    });
+
+    return emailError;
+  };
+
+  const updateName = async () => {
+    const { error: nameError } = await supabase
+      .from("account")
+      .update({ fullName })
+      .eq("user_id", user_id);
+
+    return nameError;
+  };
+
+  Promise.all([updateEmail(), updateName()]);
+
+  revalidatePath("app/account");
 };
 
-export const updatePassword = async ({ password }: PasswordForm) => {
+export const updatePassword = async (formData: FormData) => {
+  const supabase = createClient();
   const user_id = await getUser();
 
-  const { data, error } = await supabase.auth.updateUser({
-    password: password,
-    data: { hello: "world" },
-  });
+  const formFields = Object.fromEntries(formData);
+
+  // const { data, error } = await supabase.auth.updateUser({
+  //   password: password,
+  //   data: { hello: "world" },
+  // });
 };
 
 export const updatePreferencesAction = async (formData: FormData) => {
+  const supabase = createClient();
   const user_id = getUser();
+
   const formFields = Object.fromEntries(formData);
   const data = {
     darkMode: Boolean(formFields.darkMode),
@@ -43,7 +58,9 @@ export const updatePreferencesAction = async (formData: FormData) => {
 };
 
 export const submitContactFormAction = async (formData: FormData) => {
+  const supabase = createClient();
   const user_id = await getUser();
+
   const formFields = Object.fromEntries(formData);
   const { type, message } = formFields;
 
@@ -65,5 +82,6 @@ export const submitContactFormAction = async (formData: FormData) => {
 };
 
 export const logoutUser = async () => {
+  const supabase = createClient();
   let { error } = await supabase.auth.signOut();
 };
