637150d4e05c7e7f6f540c655a9e81b9938e9b78
Justin Rivera <justin_rivera9@outlook.com>
2024-10-30 22:00:19 -0600

    Created client component for form

---
+++ components/pages/account/forms/PersonalInfoForm.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { updatePersonalInfoAction } from "@/app/app/account/actions";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import FormCard from "@/components/ui/FormCard";
+import { Input } from "@/components/ui/input";
+import { PersonalInfoFormSchema } from "@/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type PersonalInfoFormProps = {
+  data: {
+    name: string;
+    email: string | undefined;
+  };
+};
+
+const PersonalInfoForm = ({ data }: PersonalInfoFormProps) => {
+  const { name, email } = data;
+
+  // SET DEFAULT VALUES TO USER OBJECT
+  // if (form field !== user object) => show update button --> means user has updated information
+
+  const form = useForm<z.infer<typeof PersonalInfoFormSchema>>({
+    resolver: zodResolver(PersonalInfoFormSchema),
+    defaultValues: {
+      fullName: name || "",
+      email: email || "",
+    },
+  });
+
+  const { formState } = form;
+
+  return (
+    <>
+      <FormCard
+        title="Personal Information"
+        description="Update your personal details."
+      >
+        <Form {...form}>
+          <form
+            action={updatePersonalInfoAction}
+            className="flex flex-col gap-2"
+          >
+            {/* FULL NAME INPUT */}
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Full Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter full name"
+                      type="text"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* EMAIL INPUT */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter email" type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {formState.isDirty && (
+              <Button type="submit" className="bg-accent text-dark w-1/3 mt-2">
+                Update
+              </Button>
+            )}
+          </form>
+        </Form>
+      </FormCard>
+    </>
+  );
+};
+
+export default PersonalInfoForm;
