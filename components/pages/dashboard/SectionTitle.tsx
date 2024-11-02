537ff5d1d963e008959eef10d5f257a543ed7b04
Justin Rivera <justin_rivera9@outlook.com>
2024-11-02 10:17:40 -0600

    Updated text size

--- components/pages/dashboard/SectionTitle.tsx
+++ components/pages/dashboard/SectionTitle.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const SectionTitle = ({ children }: { children: ReactNode }) => {
   return (
-    <h2 className="text-xl w-full flex justify-center py-2 text-accent">
+    <h2 className="text-2xl w-full flex justify-center py-2 text-accent font-bold">
       {children}
     </h2>
   );
