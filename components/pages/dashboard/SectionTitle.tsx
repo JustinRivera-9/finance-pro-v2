32f7edfbfbae0f7b5e32ed9ffb1ad51102b740d2
Justin Rivera <justin_rivera9@outlook.com>
2024-11-04 21:15:24 -0700

    Modified styles

--- components/pages/dashboard/SectionTitle.tsx
+++ components/pages/dashboard/SectionTitle.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const SectionTitle = ({ children }: { children: ReactNode }) => {
   return (
-    <h2 className="text-2xl w-full flex justify-center py-2 text-accent font-bold">
+    <h2 className="text-2xl w-full flex justify-center text-accent font-bold">
       {children}
     </h2>
   );
