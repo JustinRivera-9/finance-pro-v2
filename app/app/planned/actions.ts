045a5a88783920fb13eca7f011f092c9c14b2811
Justin Rivera <justin_rivera9@outlook.com>
2024-11-07 23:31:43 -0700

    Updating category name now updates expenses category name

--- app/app/planned/actions.ts
+++ app/app/planned/actions.ts
@@ -5,6 +5,7 @@ import { getUser } from "@/lib/supabase/actions";
 
 import { v4 as uuid } from "uuid";
 import { revalidatePath } from "next/cache";
+import { error } from "console";
 
 export const getCategories = async () => {
   const supabase = createClient();
@@ -84,10 +85,11 @@ export const deleteCategoryAction = async (id: string) => {
 
 export const updateCategoryAction = async (formData: FormData) => {
   const supabase = createClient();
+  const user_id = await getUser();
 
   const formFields = Object.fromEntries(formData);
 
-  const { isFixed, type } = formFields;
+  const { isFixed, type, oldCategory } = formFields;
   let cleanData: CategoryFormData;
 
   if (type === "income" || !isFixed) {
@@ -104,7 +106,7 @@ export const updateCategoryAction = async (formData: FormData) => {
   }
 
   try {
-    const { error } = await supabase
+    const { error: updateCategoryError } = await supabase
       .from("categories")
       .update({
         type: cleanData.type,
@@ -113,14 +115,25 @@ export const updateCategoryAction = async (formData: FormData) => {
         date: cleanData.date,
         isFixed: cleanData.isFixed,
       })
-      .eq("id", cleanData.id)
-      .select();
+      .eq("id", cleanData.id);
 
     revalidatePath("app/planned");
+
+    // Updates all expenses that match the category being updated.
+    const { error: updateExpensesError } = await supabase
+      .from("expenses")
+      .update({ category: cleanData.category })
+      .eq("category", oldCategory)
+      .eq("user_id", user_id);
+
+    if (updateCategoryError) throw Error("Error updating category");
+    if (updateExpensesError) throw Error("Error updating expense category");
 
     return { success: true, message: "Category successfully updated!" };
   } catch (error) {
     const err = error as Error;
+    console.error(err.message);
+
     return { success: false, message: err.message };
   }
 };
