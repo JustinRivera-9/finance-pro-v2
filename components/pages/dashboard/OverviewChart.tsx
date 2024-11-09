5e6c7ca0f565af0cae7d9fe32ab46d0b36e56aae
Justin Rivera <justin_rivera9@outlook.com>
2024-11-08 20:43:38 -0700

    Added check for new user message

--- components/pages/dashboard/OverviewChart.tsx
+++ components/pages/dashboard/OverviewChart.tsx
@@ -20,6 +20,12 @@ import { format } from "date-fns";
 
 export const description = "A pie chart showing all category expenses";
 
+const noExpensesMessage = (
+  <p className="mx-auto w-10/12 py-4 text-lg text-center">
+    Add expenses to see your budget overview!
+  </p>
+);
+
 const chartConfig = {
   spent: {
     label: "Category",
@@ -28,7 +34,6 @@ const chartConfig = {
 
 export const OverviewChart = ({ data }: { data: PieChartCategory[] }) => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
-
   const dirtyData = data.map((item) => {
     return { category: item.category, amount: item.spentAmount };
   });
@@ -49,63 +54,70 @@ export const OverviewChart = ({ data }: { data: PieChartCategory[] }) => {
   const month = format(new Date(), "PP").split(" ")[0];
 
   return (
-    <div className="flex-1 pb-0 pt-4 bg-card rounded-xl w-10/12">
+    <div className="flex-1 pb-0 pt-4 bg-card rounded-xl w-10/12 h-fit">
       <SectionTitle>Budget Overview</SectionTitle>
-
-      <ChartContainer
-        config={chartConfig}
-        className="mx-auto aspect-square max-h-[275px]"
-      >
-        <PieChart>
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent hideLabel />}
-          />
-          <Pie
-            data={chartData}
-            dataKey="amount"
-            nameKey="category"
-            innerRadius={60}
-            strokeWidth={5}
-            onClick={getActiveIndex}
-            paddingAngle={5}
-            activeIndex={activeIndex}
-            activeShape={({ outerRadius = 0, ...props }: PieSectorDataItem) => (
-              <Sector {...props} outerRadius={outerRadius + 20} />
-            )}
-          >
-            <Label
-              content={({ viewBox }) => {
-                if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                  return (
-                    <text
-                      x={viewBox.cx}
-                      y={viewBox.cy}
-                      textAnchor="middle"
-                      dominantBaseline="middle"
-                    >
-                      <tspan
+      {!spentTotal ? (
+        noExpensesMessage
+      ) : (
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[275px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="amount"
+              nameKey="category"
+              innerRadius={60}
+              strokeWidth={5}
+              cornerRadius={15}
+              onClick={getActiveIndex}
+              paddingAngle={5}
+              activeIndex={activeIndex}
+              activeShape={({
+                outerRadius = 0,
+                ...props
+              }: PieSectorDataItem) => (
+                <Sector {...props} outerRadius={outerRadius + 20} />
+              )}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
                         x={viewBox.cx}
                         y={viewBox.cy}
-                        className="fill-accent text-2xl font-bold"
+                        textAnchor="middle"
+                        dominantBaseline="middle"
                       >
-                        {formatCurrency(spentTotal, true)}
-                      </tspan>
-                      <tspan
-                        x={viewBox.cx}
-                        y={(viewBox.cy || 0) + 24}
-                        className="fill-light text-lg italic"
-                      >
-                        {month}
-                      </tspan>
-                    </text>
-                  );
-                }
-              }}
-            />
-          </Pie>
-        </PieChart>
-      </ChartContainer>
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-accent text-2xl font-bold"
+                        >
+                          {formatCurrency(spentTotal, true)}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-light text-lg italic"
+                        >
+                          {month}
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      )}
       {/* <CardFooter className="flex-col items-start gap-2 text-sm">
         <div className="flex gap-2 font-medium leading-none text-light/80">
           {`Trending up by $230 (5.2%) this month`}
