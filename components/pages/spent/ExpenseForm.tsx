142419a30233a94c13cb7bd954820cc6b57d616b
Justin Rivera <justin_rivera9@outlook.com>
2024-11-15 18:07:43 -0700

    Calendar iOS test

--- components/pages/spent/ExpenseForm.tsx
+++ components/pages/spent/ExpenseForm.tsx
@@ -129,7 +129,7 @@ const ExpenseForm = ({ category, expenseData }: ExpenseFormProps) => {
                     disabled={(date) =>
                       date > new Date() || date < new Date("1900-01-01")
                     }
-                    initialFocus
+                    // initialFocus
                   />
                 </PopoverContent>
               </Popover>
