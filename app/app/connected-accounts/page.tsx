7f897a51b0f14139993a5aaaa089d360790b9a81
Justin Rivera <justin_rivera9@outlook.com>
2024-11-15 18:40:17 -0700

    Updated message

--- app/app/connected-accounts/page.tsx
+++ app/app/connected-accounts/page.tsx
@@ -5,6 +5,7 @@ const ConnectedAccountsPage = () => {
   return (
     <div className="flex flex-col items-center">
       <h1>Connect Your Bank Accounts</h1>
+      <p className="text-error">Coming soon. One man army over here</p>
       {/* <PlaidLink /> */}
     </div>
   );
