47af18193d6aadbe3811468db52a9f4921423517
Justin Rivera <justin_rivera9@outlook.com>
2024-11-24 13:14:44 -0700

    Finished card design

--- components/pages/connectedAccounts/TypeSavings.tsx
+++ components/pages/connectedAccounts/TypeSavings.tsx
@@ -1,5 +1,29 @@
-const TypeSavings = () => {
-  return <div>TypeSavings</div>;
+import { AccountBase } from "plaid";
+import { formatCurrency } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+
+const TypeSavings = ({ account }: { account: AccountBase }) => {
+  const { balances, name, subtype } = account;
+
+  return (
+    <section className="flex flex-col gap-2 bg-card py-2 px-4 rounded-2xl">
+      <div className="flex items-center justify-between">
+        <p className="text-lg">{name}</p>
+        <p className="text-xs text-secondary">{subtype}</p>
+      </div>
+      <Separator className="bg-page rounded-full" />
+      <div className="flex justify-around">
+        <div className="flex flex-col">
+          <p className="text-light/70">Available</p>
+          <p className="text-lg">{formatCurrency(balances.available)}</p>
+        </div>
+        <div className="flex flex-col">
+          <p className="text-light/70">Current</p>
+          <p className="text-lg">{formatCurrency(balances.current)}</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TypeSavings;
