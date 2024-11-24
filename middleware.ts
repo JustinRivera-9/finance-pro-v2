878a3e9c28ef00a32879078bf163d67ec7fe0657
Justin Rivera <justin_rivera9@outlook.com>
2024-11-23 17:18:48 -0700

    Added NextResponse.next()

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
-import { type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  await updateSession(request);
+  NextResponse.next();
 }
 
 export const config = {
