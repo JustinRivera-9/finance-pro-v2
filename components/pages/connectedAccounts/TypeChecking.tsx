b88306eb49fa76889753609f0a27424d3729da57
Justin Rivera <justin_rivera9@outlook.com>
2024-11-24 12:48:27 -0700

    Init

--- components/pages/connectedAccounts/TypeChecking.tsx
+++ components/pages/connectedAccounts/TypeChecking.tsx
@@ -1,5 +1,16 @@
-const TypeChecking = () => {
-  return <div>TypeChecking</div>;
+import { AccountBase } from "plaid";
+import { formatCurrency } from "@/lib/utils";
+
+const TypeChecking = ({ account }: { account: AccountBase }) => {
+  const { balances, name, subtype } = account;
+
+  return (
+    <div className="bg-card py-2 px-4 rounded-2xl">
+      <p>{name}</p>
+      <p>Available {formatCurrency(balances.available)}</p>
+      <p>Current {formatCurrency(balances.current)}</p>
+    </div>
+  );
 };
 
 export default TypeChecking;
