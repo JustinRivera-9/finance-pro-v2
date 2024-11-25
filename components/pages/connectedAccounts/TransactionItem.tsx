b2d85e8660a6102fad3ccdf0a0313149ae9baa48
Justin Rivera <justin_rivera9@outlook.com>
2024-11-24 21:53:13 -0700

    Item data showing correctly

--- components/pages/connectedAccounts/TransactionItem.tsx
+++ components/pages/connectedAccounts/TransactionItem.tsx
@@ -1,14 +1,53 @@
-const TransactionItem = () => {
-  // TRANSACTION DATA
-  // account_id - use this to match transactions to specific accounts
-  // amount
-  // authorized_date
-  // merchant_name
-  // logo_url
-  // personal_finance_category.primary
-  // personal_finance_category_icon_url
-  // transaction_id
-  return <div>TransactionItem</div>;
+import {
+  capitalizePlaidCategory,
+  formatCurrency,
+  formatExpenseDate,
+} from "@/lib/utils";
+import { format } from "date-fns";
+import Image from "next/image";
+import type { Transaction } from "plaid";
+import ConfirmTransactionBtn from "./ConfirmTransactionBtn";
+
+const TransactionItem = ({ data }: { data: Transaction }) => {
+  const {
+    account_id,
+    amount,
+    authorized_date,
+    date,
+    merchant_name,
+    logo_url,
+    personal_finance_category,
+    personal_finance_category_icon_url,
+    transaction_id,
+    name,
+  } = data;
+
+  return (
+    <>
+      <section className="grid grid-cols-[0.5fr,1fr,2fr,0.5fr] gap-4 text-sm items-center py-2">
+        <Image
+          src={`${logo_url || personal_finance_category_icon_url}`}
+          alt="Logo or merchant"
+          width={50}
+          height={50}
+          className="rounded-full"
+        />
+        <div className="flex flex-col items-center truncate">
+          <p className="text-center">{formatCurrency(amount)}</p>
+          <p className="text-right">
+            {formatExpenseDate(format(authorized_date || date, "P"))}
+          </p>
+        </div>
+        <div className="flex flex-col truncate">
+          <p className="text-left">{merchant_name || name}</p>
+          <p className="text-left">
+            {capitalizePlaidCategory(personal_finance_category?.primary)}
+          </p>
+        </div>
+        <ConfirmTransactionBtn />
+      </section>
+    </>
+  );
 };
 
 export default TransactionItem;
