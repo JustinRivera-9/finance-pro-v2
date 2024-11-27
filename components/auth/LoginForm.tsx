2f671cc2e0117e2151550a91d1c40bfa8bb7211a
Justin Rivera <justin_rivera9@outlook.com>
2024-11-27 09:17:14 -0700

    Added back btn to landing page

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -15,6 +15,7 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "../ui/button";
+import BackBtn from "./BackBtn";
 
 export default function LoginForm() {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -26,84 +27,51 @@ export default function LoginForm() {
   });
 
   return (
-    <Form {...form}>
-      <form action={login} className="flex flex-col gap-4 w-10/12 mx-auto">
-        {/* EMAIL INPUT */}
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter email" type="email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div>
+      <BackBtn />
+      <Form {...form}>
+        <form action={login} className="flex flex-col gap-4 w-10/12 mx-auto">
+          {/* EMAIL INPUT */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter email" type="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        {/* PASSWORD INPUT */}
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="******" type="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex flex-col gap-2 py-2">
-          <Button className="w-full">Login</Button>
-          <div>
-            Do not have an account?{" "}
-            <Link href="signup" className="text-secondary underline">
-              Sign Up
-            </Link>{" "}
-            here
+          {/* PASSWORD INPUT */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="******" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col gap-2 py-2">
+            <Button className="w-full">Login</Button>
+            <div>
+              Do not have an account?{" "}
+              <Link href="signup" className="text-secondary underline">
+                Sign Up
+              </Link>{" "}
+              here
+            </div>
           </div>
-        </div>
-      </form>
-    </Form>
-
-    // <form className="pt-8 flex flex-col gap-4 justify-center items-stretch w-full px-8">
-    //   <div className="flex gap-4 items-center">
-    //     <Label htmlFor="email" className="text-lg">
-    //       Email:
-    //     </Label>
-    //     <Input
-    //       className="text-light"
-    //       id="email"
-    //       name="email"
-    //       type="email"
-    //       required
-    //     />
-    //   </div>
-    //   <div className="flex gap-4 items-center">
-    //     <Label htmlFor="password" className="text-lg">
-    //       Password:
-    //     </Label>
-    //     <Input
-    //       className="text-light"
-    //       id="password"
-    //       name="password"
-    //       type="password"
-    //       required
-    //     />
-    //   </div>
-    //   <div className="flex justify-center items-center gap-4">
-    //     <button
-    //       formAction={login}
-    //       className="text-dark bg-accent rounded-lg py-2 px-6"
-    //     >
-    //       Log in
-    //     </button>
-    //     <button formAction={signup}>Sign up</button>
-    //   </div>
-    // </form>
+        </form>
+      </Form>
+    </div>
   );
 }
--- components/auth/SignupForm.tsx
+++ components/auth/SignupForm.tsx
@@ -15,6 +15,7 @@ import { z } from "zod";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { signup } from "@/app/auth/login/actions";
+import BackBtn from "./BackBtn";
 
 export default function SignupForm() {
   const form = useForm<z.infer<typeof SignupSchema>>({
@@ -27,66 +28,69 @@ export default function SignupForm() {
   });
 
   return (
-    <Form {...form}>
-      <form action={signup} className="flex flex-col gap-4 w-10/12 mx-auto">
-        {/* EMAIL INPUT */}
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter email" type="email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div>
+      <BackBtn />
+      <Form {...form}>
+        <form action={signup} className="flex flex-col gap-4 w-10/12 mx-auto">
+          {/* EMAIL INPUT */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter email" type="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        {/* PASSWORD INPUT */}
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="******" type="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          {/* PASSWORD INPUT */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="******" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        {/* NAME INPUT */}
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Full Name</FormLabel>
-              <FormControl>
-                <Input placeholder="John Doe" type="text" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          {/* NAME INPUT */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="flex flex-col gap-2 py-2">
-          <Button type="submit" className="w-full">
-            Signup
-          </Button>
-          <div>
-            Already have an account?{" "}
-            <Link href="login" className="text-secondary underline">
-              Login
-            </Link>{" "}
-            here
+          <div className="flex flex-col gap-2 py-2">
+            <Button type="submit" className="w-full">
+              Signup
+            </Button>
+            <div>
+              Already have an account?{" "}
+              <Link href="login" className="text-secondary underline">
+                Login
+              </Link>{" "}
+              here
+            </div>
           </div>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+    </div>
   );
 }
