07f743b17fda00ca8cb1e21c4987b4050d243bd6
Justin Rivera <justin_rivera9@outlook.com>
2024-11-27 08:49:08 -0700

    Remoevd temp data

--- components/navigation/mobile/Header.tsx
+++ components/navigation/mobile/Header.tsx
@@ -2,14 +2,6 @@ import React from "react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-///// TEMP DATA
-const account = {
-  firstName: "John",
-  lastName: "Doe",
-  email: "johndoe@gmail.com",
-  id: 1,
-};
-
 const Header = () => {
   return (
     <header className="w-full sticky top-0 z-30 flex bg-page justify-between px-12 py-4 items-center text-slate-100">
