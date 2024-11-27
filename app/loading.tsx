cb93cd6264cc65e5dc5c2a15b2c26387b471875e
Justin Rivera <justin_rivera9@outlook.com>
2024-11-27 08:57:38 -0700

    Remoevd message for general loading fallback

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 import Spinner from "@/components/ui/Spinner";
 
 const loading = () => {
-  return <Spinner size="lg" message="Loading expenses..." />;
+  return <Spinner size="lg" />;
 };
 
 export default loading;
