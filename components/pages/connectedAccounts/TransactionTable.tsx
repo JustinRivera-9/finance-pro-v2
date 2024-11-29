2edbaa7d19e0f035a87c1ac33defb3b0f3e7ade8
Justin Rivera <justin_rivera9@outlook.com>
2024-11-29 00:55:26 -0700

    Added state to show "Confirm Transactions"

--- components/pages/connectedAccounts/TransactionTable.tsx
+++ components/pages/connectedAccounts/TransactionTable.tsx
@@ -1,6 +1,9 @@
+"use client";
 import type { AccountBase, Transaction } from "plaid";
 import TransactionItem from "./TransactionItem";
 import { Separator } from "@/components/ui/separator";
+import { useState } from "react";
+import { ApprovedTransactionItem } from "@/types/plaid";
 
 const TransactionTable = ({
   transactions,
@@ -8,23 +11,55 @@ const TransactionTable = ({
   transactions: Transaction[];
   accounts: AccountBase[];
 }) => {
+  const [approvedTransactions, setApprovedTransactions] = useState<
+    ApprovedTransactionItem[]
+  >([]);
+
+  // filters out bank related transactions i.e. credit card payments, income, etc.
   const nonBankTransactions = transactions.filter(
     (item) =>
       item.payment_channel === "online" || item.payment_channel === "in store"
   );
 
+  const handleAddTransaction = (transaction: ApprovedTransactionItem) => {
+    setApprovedTransactions((prev) => {
+      // If transaction exists then need to remove from array. If it does not then add
+      const existingTransaction = prev.find(
+        (item) => item.transaction_id === transaction.transaction_id
+      );
+
+      if (existingTransaction) {
+        return prev.filter(
+          (item) => item.transaction_id !== transaction.transaction_id
+        );
+      }
+
+      return [...prev, transaction];
+    });
+  };
+
   return (
-    <div className="flex flex-col gap-2">
-      <h1 className="text-2xl text-accent">Transactions Pending</h1>
-      <ul className="flex flex-col gap-2">
-        {nonBankTransactions.map((item) => (
-          <li className="flex flex-col gap-2" key={item.transaction_id}>
-            <TransactionItem data={item} />
-            <Separator />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {approvedTransactions.length ? (
+        <button className="fixed bottom-24 bg-secondary text-dark bg-opacity-85 font-bold px-6 py-3 rounded-full shadow-md z-50">
+          Confirm Transactions
+        </button>
+      ) : null}
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl text-accent">Transactions Pending</h1>
+        <ul className="flex flex-col gap-2">
+          {nonBankTransactions.map((item) => (
+            <li className="flex flex-col gap-2" key={item.transaction_id}>
+              <TransactionItem
+                data={item}
+                addTransaction={handleAddTransaction}
+              />
+              <Separator />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
