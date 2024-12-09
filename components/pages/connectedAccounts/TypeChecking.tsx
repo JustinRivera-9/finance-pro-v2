ac94e10b66a5b7935ba6a39d50c73f4a8086ddf0
Justin Rivera <justin_rivera9@outlook.com>
2024-12-08 21:57:07 -0700

    Updated styles for account type

--- components/pages/connectedAccounts/TypeChecking.tsx
+++ components/pages/connectedAccounts/TypeChecking.tsx
@@ -1,15 +1,28 @@
 import { AccountBase } from "plaid";
 import { formatCurrency } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
 
 const TypeChecking = ({ account }: { account: AccountBase }) => {
   const { balances, name, subtype } = account;
 
   return (
-    <div className="bg-card py-2 px-4 rounded-2xl">
-      <p>{name}</p>
-      <p>Available {formatCurrency(balances.available)}</p>
-      <p>Current {formatCurrency(balances.current)}</p>
-    </div>
+    <section className="flex flex-col gap-2 bg-card py-2 px-4 rounded-2xl">
+      <div className="flex items-center justify-between">
+        <p className="text-lg">{name}</p>
+        <p className="text-xs text-secondary">{subtype}</p>
+      </div>
+      <Separator className="bg-page rounded-full" />
+      <div className="flex justify-around">
+        <div className="flex flex-col">
+          <p className="text-light/70">Available</p>
+          <p className="text-lg">{formatCurrency(balances.available)}</p>
+        </div>
+        <div className="flex flex-col">
+          <p className="text-light/70">Current</p>
+          <p className="text-lg">{formatCurrency(balances.current)}</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
