1859e71eb3b5ef289666ee4caac40a40c1b3d844
Justin Rivera <justin_rivera9@outlook.com>
2024-12-09 20:07:22 -0700

    Updated styles and added icon

--- components/pages/landing/CallToAction.tsx
+++ components/pages/landing/CallToAction.tsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
 import SignupBtn from "./SignupBtn";
+import Image from "next/image";
 
 const CallToAction = () => {
   return (
-    <div className="flex flex-col gap-4 bg-card p-4 rounded-xl">
-      <h1 className="text-3xl text-accent">
-        Take back control of your finances
-      </h1>
-      <p className="text-lg">
-        Start managing your personal finance with ease and confidence by signing
-        up today!
-      </p>
+    <div className="flex flex-col gap-4 rounded-xl">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl">Start your journey today!</h1>
+        <Image
+          alt="hand grabbing money"
+          src="/features/hand-grabbing-money.png"
+          width={125}
+          height={125}
+        />
+      </div>
       <Link
         href="/auth/signup"
-        className="bg-secondary w-fit py-2 px-4 rounded-lg text-dark font-semibold"
+        className="bg-accent text-lg text-center w-full py-2 px-4 rounded-lg text-dark font-semibold mx-auto"
       >
-        Sign Up
+        Sign Up Today
       </Link>
     </div>
   );
