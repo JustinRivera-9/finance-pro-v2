9c78dc85fa0abd1b7524edc24d9b6ef39d117cad
Justin Rivera <justin_rivera9@outlook.com>
2024-12-09 20:18:50 -0700

    Updates styles

--- components/pages/landing/Benefits.tsx
+++ components/pages/landing/Benefits.tsx
@@ -1,18 +1,24 @@
 import { Box } from "lucide-react";
+import Image from "next/image";
 
 const Benefits = () => {
   return (
-    <div className="flex flex-col gap-4 text-center">
-      <h1 className="text-3xl text-accent text-center">
-        Why Personal <br />
-        Finance Matters
-      </h1>
-      <h2 className="text-lg">
+    <div className="flex flex-col gap-2 text-center">
+      <div className="flex justify-center items-center">
+        <Image
+          alt="light bulb with question marks"
+          src="/features/light-bulb.png"
+          width={100}
+          height={100}
+        />
+        <h1 className="text-4xl text-center text-light">Why it matters</h1>
+      </div>
+      {/* <h2 className="text-lg text-light/60">
         Financial health is essential for creating a secure and balanced future,
         allowing you to live with confidence and make informed financial
         decisions.
-      </h2>
-      <ul className="flex flex-col gap-2">
+      </h2> */}
+      <ul className="flex flex-col gap-2 pt-4">
         <li className="flex gap-4">
           <Box size={24} className="text-secondary" />
           Build a secure financial future
