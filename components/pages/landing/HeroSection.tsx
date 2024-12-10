49c8b9f81c51c2ce1b9a44a992a5bd94542efa6d
Justin Rivera <justin_rivera9@outlook.com>
2024-12-09 19:58:19 -0700

    Updated styles for hero section

--- components/pages/landing/HeroSection.tsx
+++ components/pages/landing/HeroSection.tsx
@@ -1,14 +1,24 @@
+import Image from "next/image";
 import SignupBtn from "./SignupBtn";
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-4xl text-accent">Personal Finance Made Easy</h1>
-      <p className="text-lg">
-        Our app streamlines the management of your personal finances through
-        budget tracking, bank connectivity, and robust tools for monitoring
-        investments and loans, empowering you to stay laser-focused on your
-        financial goals.
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between items-center">
+        <div className="text-left">
+          <p className="text-3xl">Finance</p>
+          <p className="text-4xl text-accent">Made Easy</p>
+        </div>
+        <Image
+          alt="hero section background"
+          src="/features/dollar-icon-1.png"
+          width={125}
+          height={125}
+        />
+      </div>
+      <p className="text-lg pb-4">
+        Simplified budget tracking, bank integration, and investment monitoring
+        for goal-focused management
       </p>
       <SignupBtn />
     </div>
--- components/pages/landing/SignupBtn.tsx
+++ components/pages/landing/SignupBtn.tsx
@@ -4,9 +4,9 @@ const SignupBtn = () => {
   return (
     <Link
       href="/auth/signup"
-      // className="bg-accent w-fit py-2 px-4 rounded-lg text-dark font-semibold"
+      className="bg-accent w-fit py-2 px-4 rounded-lg text-dark font-semibold"
       // className="border border-accent w-fit py-2 px-4 rounded-lg text-accent font-semibold"
-      className="border border-secondary w-fit py-2 px-4 rounded-lg text-secondary font-semibold"
+      // className="border border-secondary w-fit py-2 px-4 rounded-lg text-secondary font-semibold"
       // className="bg-secondary w-fit py-2 px-4 rounded-lg text-dark font-semibold"
     >
       Sign Up
