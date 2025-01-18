a13ac5a2c90f01aafbf88f7a3fd4c28c514d6b82
Justin Rivera <justin_rivera9@outlook.com>
2025-01-18 10:36:29 -0700

    Added formatted filter string to pass into filter functions

--- lib/hooks.tsx
+++ lib/hooks.tsx
@@ -1,9 +1,11 @@
 import { useSearchParams } from "next/navigation";
+import { formatMonthAndYear } from "./utils";
 
 export const useParamFilters = () => {
   const searchParams = useSearchParams();
   const month = searchParams.get("month");
   const year = searchParams.get("year");
+  const formattedFilter = formatMonthAndYear(month!, year!);
 
-  return { month, year };
+  return { month, year, formattedFilter };
 };
