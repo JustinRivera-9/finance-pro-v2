a737c4aa5128e65f602c9bb5b7d5a0f2ae4fe56f
Justin Rivera <justin_rivera9@outlook.com>
2025-01-19 13:38:31 -0700

    Added expense form to drawer

--- components/pages/budget/spent/CategoryDrawer.tsx
+++ components/pages/budget/spent/CategoryDrawer.tsx
@@ -7,14 +7,15 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { type ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import { Separator } from "@/components/ui/separator";
 import { formatCurrency } from "@/lib/utils";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
+import ExpenseForm from "../../spent/ExpenseForm";
 
 type CategoryDrawerProps = {
-  children: ReactNode;
+  children?: ReactNode;
   triggerLabel: string | ReactNode;
   totalSpent: number;
   plannedAmount: number;
@@ -28,11 +29,30 @@ const CategoryDrawer = ({
   plannedAmount,
   triggerLabel,
 }: CategoryDrawerProps) => {
+  const [showForm, setShowForm] = useState<boolean>(false);
+  const handleClick = () => setShowForm((prev) => !prev);
+
+  // Expense form content
+  if (showForm) {
+    return (
+      <Drawer>
+        <DrawerTrigger asChild>{triggerLabel}</DrawerTrigger>
+        <DrawerContent className="border-dark pt-2 pb-6 h-[65%] z-50">
+          <DrawerHeader className="text-light mx-auto w-full">
+            <DrawerTitle className="flex justify-around items-center font-semibold text-center">
+              <p className="text-2xl">Add {category} Expense</p>
+            </DrawerTitle>
+          </DrawerHeader>
+          <ExpenseForm category={category} setShowForm={setShowForm} />
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  // Category overview content
   return (
     <Drawer>
-      <DrawerTrigger className="" asChild>
-        {triggerLabel}
-      </DrawerTrigger>
+      <DrawerTrigger asChild>{triggerLabel}</DrawerTrigger>
       <DrawerContent className="border-dark pt-2 pb-6 h-[65%] z-50">
         <DrawerHeader className="text-light mx-auto w-full">
           <DrawerTitle className="flex justify-around items-center font-semibold text-center">
@@ -48,7 +68,9 @@ const CategoryDrawer = ({
           </DrawerTitle>
         </DrawerHeader>
         <ScrollArea className="w-full mb-2">{children}</ScrollArea>
-        <Button className="w-1/2 mx-auto mt-3">Add Expense</Button>
+        <Button className="w-1/2 mx-auto mt-3" onClick={handleClick}>
+          New Expense
+        </Button>
       </DrawerContent>
     </Drawer>
   );
