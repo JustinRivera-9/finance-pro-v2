88b93a9db69b71dab852bb320066af3ea44be04b
Justin Rivera <justin_rivera9@outlook.com>
2025-01-21 21:32:13 -0700

    Updated styling

--- components/pages/budget/planned/CategoryColumn.tsx
+++ components/pages/budget/planned/CategoryColumn.tsx
@@ -14,8 +14,8 @@ const CategoryColumn: React.FC<CategoryColumnProps> = ({ category, title }) => {
 
   return (
     <div className="flex flex-col gap-2 w-full">
-      <div className="flex w-full px-10 mx-auto justify-between items-baseline font-semibold">
-        <p className="text-accent text-2xl">{title}</p>
+      <div className="flex w-full px-2 mx-auto justify-between items-baseline font-semibold">
+        <p className="text-light text-2xl">{title}</p>
         <p className="text-light/70 text-xl">{formatCurrency(total)}</p>
       </div>
       <ul className="flex flex-col flex-wrap gap-4 justify-start mx-auto w-full">
