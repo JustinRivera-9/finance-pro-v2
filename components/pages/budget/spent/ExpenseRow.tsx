1b99506c162df33879e2ba597edc6242333fb215
Justin Rivera <justin_rivera9@outlook.com>
2025-01-25 10:13:55 -0700

    Fixed column width issues across iphone and android

--- components/pages/budget/spent/ExpenseRow.tsx
+++ components/pages/budget/spent/ExpenseRow.tsx
@@ -19,24 +19,25 @@ const ExpenseRow = ({
     return (
       <section
         key={expense.id}
-        className="grid grid-cols-[20%_10%_auto] gap-4 text-left w-full py-3 border-t border-light/30"
+        className="grid grid-cols-[20%_13%_auto] gap-4 border-b w-full border-light/30 text-left py-3"
       >
         <p>{expense.date}</p>
         <p>{formatCurrency(expense.amount, true)}</p>
-        <p className="truncate w-48">{expense.description}</p>
+        <p className="truncate">{expense.description}</p>
       </section>
     );
   }
 
   return (
-    <section className="flex justify-between border-t w-full border-light/30 text-left py-3 px-3">
-      <div key={expense.id} className="grid grid-cols-[20%_15%_auto] gap-4">
-        <p>{expense.date}</p>
-        <p>{formatCurrency(expense.amount, true)}</p>
-        <p className="truncate w-48">{expense.description}</p>
-      </div>
+    <div
+      key={expense.id}
+      className="grid grid-cols-[20%_13%_auto_5%] gap-4 border-b w-full border-light/30 text-left py-3 px-4"
+    >
+      <p>{expense.date}</p>
+      <p>{formatCurrency(expense.amount, true)}</p>
+      <p className="truncate">{expense.description}</p>
       <ActionBtn expense={expense} expenseToEdit={handleEditExpense} />
-    </section>
+    </div>
   );
 };
 
