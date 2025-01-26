39d6e476d3b4430564f06aecb91eb961626c346a
Justin Rivera <justin_rivera9@outlook.com>
2025-01-26 16:17:27 -0700

    init

---
+++ app/api/plaid/sync-aupdates-available/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  //   const { accounts, added, modified, removed, cursor } =
+  //     await fetchTransactions(accessToken, item.cursor, itemId, user);
+}
