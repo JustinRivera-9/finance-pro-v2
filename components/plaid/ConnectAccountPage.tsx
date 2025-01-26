b044753f38cd40dea25e0ac7f6677933fcd2de9f
Justin Rivera <justin_rivera9@outlook.com>
2025-01-26 15:41:49 -0700

    Updated styling

--- components/plaid/ConnectAccountPage.tsx
+++ components/plaid/ConnectAccountPage.tsx
@@ -5,9 +5,11 @@ const ConnectAccountPage = async () => {
   const user = (await getUser()) as string;
 
   return (
-    <div className="flex flex-col items-center">
-      <h1>Connect Your Bank Accounts</h1>
-      <p className="text-error">Coming soon. One man army over here</p>
+    <div className="flex flex-col items-center gap-4 px-4 text-center">
+      <h1 className="text-2xl text-accent">Connect Bank Accounts</h1>
+      <h2 className="text-lg text-light/70">
+        Connect a bank below to get started!
+      </h2>
       <ConnectAccountBtn user={user} variant="primary" />
     </div>
   );
