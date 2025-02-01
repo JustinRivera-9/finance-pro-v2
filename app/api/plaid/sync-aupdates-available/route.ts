9fe5207a1b241da5ddd3270a94fce989dcdfbc65
Justin Rivera <justin_rivera9@outlook.com>
2025-02-01 12:48:58 -0700

    Removed unused routes for now

--- app/api/plaid/sync-aupdates-available/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-
-export async function POST(req: NextRequest) {
-  //   const { accounts, added, modified, removed, cursor } =
-  //     await fetchTransactions(accessToken, item.cursor, itemId, user);
-}
