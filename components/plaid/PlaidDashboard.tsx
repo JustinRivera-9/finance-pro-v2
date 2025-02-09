4c4867cad98e1da1e3550893dc75df68ac5a51a6
Justin Rivera <justin_rivera9@outlook.com>
2025-02-08 23:42:38 -0700

    Updated how plaid handles and syncs database

--- components/plaid/PlaidDashboard.tsx
+++ components/plaid/PlaidDashboard.tsx
@@ -2,37 +2,42 @@ import { syncTransactions } from "@/lib/plaid/actions";
 import AccountSection from "../pages/connectedAccounts/AccountSection";
 import TransactionTable from "../pages/connectedAccounts/TransactionTable";
 import { getCategories } from "@/app/app/budget/actions";
-import { PlaidItemData, SyncTransactionsData } from "@/types/plaid";
+import { ApprovedTransactionItem, PlaidItemData } from "@/types/plaid";
 import {
+  getAccounts,
   updateAccounts,
   updateTransactionCursor,
 } from "@/app/app/connected-accounts/actions";
+import { getTransactions } from "@/lib/supabase/transactions/actions";
+import { AccountBase } from "plaid";
 
 type PlaidDashboardProps = { item: PlaidItemData; user: string };
 
 const PlaidDashboard = async ({ item, user }: PlaidDashboardProps) => {
   const { access_token, item_id, transaction_cursor } = item;
-  const transactionData: SyncTransactionsData = await syncTransactions(
+  const { cursor } = await syncTransactions(
     access_token,
     transaction_cursor,
     item_id,
     user
   );
 
-  // Update data in database
-  updateTransactionCursor(transactionData.cursor, item_id);
-  updateAccounts(transactionData.accounts, item_id);
+  updateTransactionCursor(cursor, item_id);
 
-  const categories = await getCategories();
+  const [transactions, categories, accounts] = await Promise.all([
+    getTransactions(user),
+    getCategories(),
+    getAccounts(user),
+  ]);
 
   return (
     <div className="flex flex-col gap-4 items-center text-center py-2 px-4">
-      {/* <AccountSection accounts={transactionData.accounts} />
+      <AccountSection accounts={accounts as AccountBase[]} />
       <TransactionTable
-        transactions={added}
-        accounts={accounts}
+        transactions={transactions as ApprovedTransactionItem[]}
+        accounts={accounts as AccountBase[]}
         categories={categories as any[]}
-      /> */}
+      />
     </div>
   );
 };
