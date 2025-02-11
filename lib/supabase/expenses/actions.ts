553995efbee8cf1a918a315af4e481fbcb7cba75
Justin Rivera <justin_rivera9@outlook.com>
2025-02-10 22:14:59 -0700

    Added function to update expenses with modified transactions

--- lib/supabase/expenses/actions.ts
+++ lib/supabase/expenses/actions.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@/lib/supabase/server";
 import { ApprovedTransactionItem } from "@/types/plaid";
 import { getUser } from "../actions";
+import { capitalizePlaidCategory } from "@/lib/utils";
 
 export const getExpenses = async (user: string) => {
   const supabase = createClient();
@@ -25,11 +26,41 @@ export const updateExpenses = async (
   expenses: ApprovedTransactionItem[],
   user: string
 ) => {
-  const supabase = createClient();
-  //   console.log(expenses);
-  //   console.log(user);
   try {
+    const supabase = createClient();
     if (!expenses.length) return;
+
+    const { data, error } = await supabase
+      .from("expenses")
+      .upsert(
+        expenses.map((expense) => {
+          return {
+            transaction_id: expense.transaction_id,
+            user_id: user,
+            account_id: expense.account_id,
+            amount: expense.amount,
+            authorized_date: expense.authorized_date,
+            date: expense.date,
+            merchant_name: expense.merchant_name,
+            logo_url: expense.logo_url,
+            personal_finance_category: capitalizePlaidCategory(
+              expense.personal_finance_category
+            ),
+            personal_finance_category_icon_url:
+              expense.personal_finance_category_icon_url,
+            name: expense.name,
+            description: expense.name || (expense.merchant_name as string),
+            payment_channel: expense.payment_channel,
+          };
+        })
+      )
+      .select();
+
+    if (error)
+      throw Error(
+        `There was an issue updating expenses with modified transactions. Error message: ${error.message}`
+      );
+    return data;
   } catch (err) {
     const error = err as Error;
     console.log(error.message);
