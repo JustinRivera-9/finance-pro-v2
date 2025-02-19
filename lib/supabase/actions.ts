d4e45969cf37794d5ae29479459d60c7dde6c6f2
Justin Rivera <justin_rivera9@outlook.com>
2025-02-18 20:11:45 -0700

    Added error log for runtime auth issue

--- lib/supabase/actions.ts
+++ lib/supabase/actions.ts
@@ -8,10 +8,11 @@ export const getUser = async () => {
   try {
     const {
       data: { user },
+      error,
     } = await supabase.auth.getUser();
 
     if (!user?.id) throw Error("There was a problem getting user ID");
-
+    console.log("Error:", error);
     return user.id;
   } catch (err) {
     const error = err as Error;
